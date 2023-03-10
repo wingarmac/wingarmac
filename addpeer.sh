e1f9aa81dccf80820222f1cef4f4ba625568e783
Tony, Martin Berbel <wingarmac@gmail.com>
2023-03-10 18:42:14 +0100

    Update addpeer.sh

--- addpeer.sh
+++ addpeer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+# Install qrencode if necessary
 REQUIRED_PKG="qrencode"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 echo Checking for $REQUIRED_PKG: "$PKG_OK"
@@ -7,19 +9,24 @@ if [ "" = "$PKG_OK" ]; then
   apt install -y $REQUIRED_PKG
 fi
 
+pubkey_ubserv=$(cat /etc/wireguard/pubkey_ubserv)
+pubkey_ubcynt=$(awk '/PublicKey/{print $3; exit}' /etc/wireguard/wg0.conf)
+
+
 # Check the existing Wireguard configuration file for assigned IP addresses
 assigned_ips=($(grep -Po '(\d{1,3}\.){3}\d{1,3}/\d{1,2}' /etc/wireguard/wg0.conf | cut -d '/' -f 1))
 
-# Calculate the largest available VPN number between 5 and 255
-largest_available_vpn_number=$(seq 5 255 | grep -vE $(echo "${assigned_ips[@]}" | tr ' ' '|') | tail -n 1)
+# Calculate the largest range of available VPN numbers
+available_vpn_numbers=$(seq 5 255 | grep -vE $(echo "${assigned_ips[@]}" | tr ' ' '|'))
+largest_range=$(echo "${available_vpn_numbers[@]}" | awk '{print $NF-4}')
 
 # Ask for the new peer's name
 read -p "What is the name for the new peer? " name_host
 
-# Prompt the user for a VPN number that is not already assigned
-read -p "What is the VPN number for the new peer? (between 5 and $largest_available_vpn_number) " vpn_number
-while [[ ! $vpn_number =~ ^[0-9]+$ ]] || [[ $vpn_number -lt 5 ]] || [[ $vpn_number -gt $largest_available_vpn_number ]]; do
-  read -p "Invalid input. Please enter a number between 5 and $largest_available_vpn_number: " vpn_number
+# Prompt the user for a VPN number within the largest available range
+read -p "What is the VPN number for the new peer? (between $largest_range and 255) " vpn_number
+while [[ $vpn_number -lt $largest_range || $vpn_number -gt 255 ]]; do
+  read -p "Invalid VPN number. Please choose a number between $largest_range and 255. " vpn_number
 done
 
 # Calculate the new peer's IP address
@@ -35,13 +42,13 @@ PrivateKey = $(wg genkey)
 Address = $ip_address/32
 
 [Peer]
-PublicKey = $(wg pubkey)
+PublicKey = pubkey_ubserv
 Endpoint = 194.78.208.195:51515
 AllowedIPs = 10.5.5.1/32
 PersistentKeepalive = 21
 
 [Peer]
-PublicKey = $public_key
+PublicKey = pubkey_ubcynt
 Endpoint = 194.78.208.195:8023
 AllowedIPs = $ip_address/32
 PersistentKeepalive = 21
@@ -60,21 +67,22 @@ if [[ "$peer_type" == "mobile" ]]; then
 
   # Add the new peer to the Wireguard configuration file
   cat >> /etc/wireguard/wg0.conf <<EOF
-
+[Peer]
+PublicKey = $public_key
+Endpoint = 194.78.208.195:8023
+AllowedIPs = $ip_address/32
+PersistentKeepalive = 21
 
 EOF
 else
   # Add the new peer to the Wireguard configuration file
   cat >> /etc/wireguard/wg0.conf <<EOF
 [Peer]
-PublicKey = $(wg pubkey)
+PublicKey = pubkey_$name_host
 AllowedIPs = $ip_address/32
 
 EOF
 fi
 
 # Print the configuration for the new peer
-echo "Wireguard configuration for $name_host:"
-cat ./$name_host/wg0.conf
-
-#
+echo "Wireguard configuration for $name
